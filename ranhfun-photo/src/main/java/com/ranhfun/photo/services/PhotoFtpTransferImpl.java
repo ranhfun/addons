67997c606ff356721ca6abcc8c095d236c6ff60c
virh <virgil.hong@aliyun.com>
2018-09-05 15:24:56 +0800

    update jar dependencies

--- ranhfun-photo/src/main/java/com/ranhfun/photo/services/PhotoFtpTransferImpl.java
+++ ranhfun-photo/src/main/java/com/ranhfun/photo/services/PhotoFtpTransferImpl.java
@@ -3,8 +3,8 @@ package com.ranhfun.photo.services;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.Symbol;
 
-import com.ranhfun.ftp.FileInfo;
-import com.ranhfun.ftp.FtpToolImpl;
+//import com.ranhfun.ftp.FileInfo;
+//import com.ranhfun.ftp.FtpToolImpl;
 import com.ranhfun.photo.PhotoConstants;
 
 public class PhotoFtpTransferImpl implements PhotoFtpTransfer {
@@ -18,44 +18,44 @@ public class PhotoFtpTransferImpl implements PhotoFtpTransfer {
 	}
 	
 	public boolean transfer(String sourceFloder, String sourcePath, String distPath) {
-		FtpToolImpl tool = new FtpToolImpl();
-		if(tool.init())
-		{
-			if(tool.connect())
-			{
-				 try {
-					FileInfo file = new FileInfo();
-					file.setDir(sourceFloder);//本地路径
-					file.setFileName(sourcePath);//本地文件名
-					file.setFtpFilePath(ftpFloder + distPath);//服务器路径
-					tool.setFileName(file);
-					tool.putFile();//开始传送
-					tool.close();//关闭连接
-				 } catch (Exception  e) {
-					// transfer fail
-					return false;
-				}
-			}
-		}
+//		FtpToolImpl tool = new FtpToolImpl();
+//		if(tool.init())
+//		{
+//			if(tool.connect())
+//			{
+//				 try {
+//					FileInfo file = new FileInfo();
+//					file.setDir(sourceFloder);//本地路径
+//					file.setFileName(sourcePath);//本地文件名
+//					file.setFtpFilePath(ftpFloder + distPath);//服务器路径
+//					tool.setFileName(file);
+//					tool.putFile();//开始传送
+//					tool.close();//关闭连接
+//				 } catch (Exception  e) {
+//					// transfer fail
+//					return false;
+//				}
+//			}
+//		}
 		return true;
 	}
 
 	public boolean delete(String distPath) {
 		boolean flag = false;
-		FtpToolImpl tool = new FtpToolImpl();
-		if(tool.init())
-		{
-			if(tool.connect())
-			{
-				 try {
-					flag = tool.delFile(ftpFloder + distPath);//开始传送
-					tool.close();//关闭连接
-				 } catch (Exception  e) {
-					// transfer fail
-					return false;
-				}
-			}
-		}
+//		FtpToolImpl tool = new FtpToolImpl();
+//		if(tool.init())
+//		{
+//			if(tool.connect())
+//			{
+//				 try {
+//					flag = tool.delFile(ftpFloder + distPath);//开始传送
+//					tool.close();//关闭连接
+//				 } catch (Exception  e) {
+//					// transfer fail
+//					return false;
+//				}
+//			}
+//		}
 		return flag;
 	}
 
